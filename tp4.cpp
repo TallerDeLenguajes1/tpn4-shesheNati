5b21e0b0f3449cf92a51ec1b7e0458cbce2baed9
shesheNati <acosta_jessica@live.com>
2020-07-13 16:08:37 -0300

    agregando funciones de busqueda

--- tp4.cpp
+++ tp4.cpp
@@ -12,15 +12,21 @@ typedef struct{
 
 /***********************  PROTOTIPO *****************************/
 void AltaDeTareas(S_Tarea **tar, int cantT);
-void OrganizandoTareas(S_Tarea **tareasT, S_Tarea **tareasP, S_Tarea **tareasR, , int cantT);
-void MostrarTareas(S_Tarea tar,int cantT);
+void OrganizandoTareas(S_Tarea **tareasT, S_Tarea **tareasP, S_Tarea **tareasR, int cantT);
+void MostrarTareas(S_Tarea **tar,int cantT);
+S_Tarea *BuscarTareaXPalabra(S_Tarea **tar,int cantT ,char *palabra);
+S_Tarea *BuscarTareaID(S_Tarea **tar,int cantT);
+
+
+
 /*********************** PROGRAMA PRINCIPAL ************************/
 int main()
 {
     int cant_tareas, ID;
+    char palabra[30];
     S_Tarea **tareas_realizadas;//arreglos de punteros dobles de tipo estructura S_Tarea
     S_Tarea **tareas_pendientes;
-    S_Tarea **tareas_totales
+    S_Tarea **tareas_totales;
 
     printf("Bienvenido al Control de Tareas\n");
     printf("Ingrese la cantidad de tareas a cargar:"); 
@@ -33,6 +39,28 @@ int main()
     AltaDeTareas(tareas_totales, cant_tareas);
     OrganizandoTareas(tareas_totales, tareas_pendientes, tareas_realizadas, cant_tareas);
     MostrarTareas(tareas_realizadas, cant_tareas);
+
+    S_Tarea *busca_palabra_clave = BuscarTareaXPalabra(tareas_realizadas,cant_tareas,palabra);
+    if (busca_palabra_clave == NULL) 
+    {
+        printf("Sin coincidencias con esa palabra clave en las tareas %s\n", palabra);
+    }
+    else
+    {
+        printf("Resultado de la palabra clave: ID: %d Duracion: %d\n",busca_palabra_clave->TareaID, busca_palabra_clave->Duracion);
+    }
+
+
+    S_Tarea *buscar_tarea = BuscarTareaID(tareas_realizadas,cant_tareas);
+    if(buscar_tarea == NULL)
+    {
+        printf("No se encontro ninguna tarea con ese ID: %d\n",ID);
+    }
+    else
+    {
+        printf("La tarea con ID %d es: %s\n",ID,buscar_tarea->Descripcion);
+    }
+
 
     scanf(" %c");
     return 0;
@@ -87,7 +115,7 @@ void OrganizandoTareas(S_Tarea **tareasT, S_Tarea **tareasP, S_Tarea **tareasR, 
 
 }
 
-void MostrarTareas(S_Tarea tar,int cantT)
+void MostrarTareas(S_Tarea **tar,int cantT)
 {
     for(int i = 0; i < cantT; i++)
     {
@@ -96,7 +124,45 @@ void MostrarTareas(S_Tarea tar,int cantT)
             printf("\nID de tarea: %d\n",tar[i]->TareaID);
             printf("Descripcion: %s\n",tar[i]->Descripcion);
             printf("Duracion: %d\n",tar[i]->Duracion);
-            printf("--------------------------------------\n");
+            printf("________________________________\n");
         }
     }
 }
+
+S_Tarea *BuscarTareaXPalabra(S_Tarea **tar,int cantT ,char *palabra)
+{
+    printf("Ingrese la palabra clave de la tarea que desea buscar:\n");
+    scanf("%s ",palabra);
+
+    for (int i = 0; i < cantT; i++)
+    {
+        if (tar[i] != NULL)
+        {
+            if (strcmp(tar[i]->Descripcion,palabra) == 0)
+            {
+                return tar[i];
+            }
+        } 
+    }
+    return NULL;
+}
+
+S_Tarea *BuscarTareaID(S_Tarea **tar,int cantT)
+{
+    int aux_id;
+
+    printf("Ingrese id de la tarea a buscar\n");
+    scanf("%d",&aux_id);
+    for (int i = 0; i < cantT; i++)
+    {
+        if (tar[i] != NULL) 
+        {
+            if (tar[i]->TareaID == aux_id)
+            {
+                return tar[i];
+            }
+        }
+    }
+    return NULL;
+}
+
